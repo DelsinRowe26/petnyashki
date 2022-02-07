b4fcb66650ab33ada46f1148e7df78b175ef0fb9
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2022-02-07 11:55:24 +0700

    Chto-to rabotaet a chto-to net

--- petnyashki/Source.cpp
+++ petnyashki/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n, m, c = 0, my_num, sm_num = 0, my_num_two, sm_num_two = 0, b = 0, f = 0, d = 0, e = 0, g = 0, h = 0, k = 0;
+    int n, m, c = 0, my_num, sm_num = 0, my_num_two, sm_num_two = 0, b = 0, f = 0, d = 0, e = 0, g = 0, h = 0, k = 0, temp1, temp2;
     cin >> n >> m;
     int a[30][30];
 
@@ -25,6 +25,13 @@ int main() {
             if (a[i][j] % 10 == a[i][j]) cout << "   ";
             else if (a[i][j] % 100 == a[i][j]) cout << "  ";
             else if (a[i][j] % 1000 == a[i][j]) cout << " ";
+            if (my_num > my_num_two)
+            {
+                temp1 = my_num;
+                temp2 = my_num_two;
+                my_num = temp2;
+                my_num_two = temp1;
+            }
             if (my_num == a[i][j]) {
                 //sm_num = a[i][j];
                 b = i;
@@ -37,13 +44,27 @@ int main() {
                 e = j;
                 h++;
             }
-            if (i == b && j == f && g >= 1 && my_num_two > 0 && my_num_two <= m * n && ((e - j >= 1 && e - j >= 0) || (d - i <= 1)) && (((e - j <= 1 && e - j >=0) || (d - i >= 1 && d - i >= 0)))) {
-                a[i][j] = my_num_two;
-                if (i == d && j == e && h >= 1 && my_num > 0 && my_num <= m * n && ((j - f <= 1 && j - f >= 0) || (i - b >= 1/* && i - b >= 0 */)) && ((j - f >= 1 && j - f >= 0) || (i - b <= 1 /*&& i - b >= 0*/))) {
-                    a[b][f] = my_num;
-                }
-            }
             
+            /*if (i == b && j == f && g >= 1 && my_num_two > 0 && my_num_two <= m * n && ((e - j >= 1 || e - j >= 0) || (d - i <= 1)) && (((e - j <= 1 || e - j >=0) || (d - i >= 1 || d - i >= 0)))) {
+                a[i][j] = my_num_two;
+                
+            }
+            if (i == d && j == e && h >= 1 && my_num > 0 && my_num <= m * n && ((j - f <= 1 || j - f >= 0) || (i - b >= 1/* && i - b >= 0 )) && ((j - f >= 1 || j - f >= 0) || (i - b <= 1 /*&& i - b >= 0))) {
+                a[b][f] = my_num;
+            }*/
+            if (i == b && j == f && g >= 1 && my_num_two > 0 && my_num_two <= m * n && (d - i == 0 || d - i == 1 || d - i == -1) && (e - j == 0 || e - j == 1 || e - j == -1))//индексы первого числа
+            {
+                a[i][j] = my_num_two;
+                k++;
+            }
+            if (i == d && j == e && h >= 1 && (i - b == 1 || i - b == -1 || i - b == 0) && (j - f == 1 || j - f == -1 || j - f == 0) && k >= 1)//индексы второго числа
+            {
+                a[i][j] = my_num;
+            }
+            /*if (i == d && j == e && h >= 1 && (d - b == 1 || d - b == -1 || d - b == 0) && (e - f == 1 || e - f == -1 || e - f == 0))//индексы второго числа
+            {
+                a[i][j] = my_num;
+            }*/
             cout << a[i][j];
         }
         cout << endl;
